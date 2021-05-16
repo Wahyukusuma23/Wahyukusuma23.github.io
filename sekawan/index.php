e8072c738596051cc7737ed0cb3636e835ea7dc6
kade <potter.kade@gmail.com>
2021-05-16 19:30:39 +0700

    first upload

---
+++ sekawan/index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anime Center</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+    <nav>
+        <div class="container">
+            <a href="" id="a-home">
+                <span class="material-icons">home</span>
+            </a>
+            <ul class="nav-menu">
+                <li>Update</li>
+                <li>Featured</li>
+                <li>Anime List</li>
+                <li>Favorite</li>
+            </ul>
+            <button class="menu-mobile" style="border:none;background:transparent;color:white"><span class="material-icons">
+menu
+</span></button>
+        </div>
+    </nav>
+    <div class="container ctn">
+        <div class="left">
+            <div class="video-wrap">
+                <div class="video">
+                    <img src="play.png" alt="">
+                    Play Trailer
+                </div>
+            </div>
+            <div class="v-page">
+                <ul>
+                    <li>01</li>
+                    <li>02</li>
+                    <li><span class="material-icons">keyboard_arrow_right</span></li>
+                </ul>
+            </div>
+            <h5 class="anime-title">Anime Title</h5>
+            <div class="act-button">
+                <button class="subscribe">Subscribe</button>
+                <button><span class="material-icons">thumb_up_alt</span></button>
+                <button><span class="material-icons">thumb_down</span></button>
+            </div>
+            <div class="barrier"></div>
+            <button class="rating">Rating PG 13</button>
+            <div class="info-left">
+                <p>Author : User Test</p>
+                <p>Studio : MAPALA</p>
+                <p>Producers : Kadokawa</p>
+                <p>Genre :</p>
+                <button>Action</button>
+                <button>Horror</button>
+            </div>
+        </div>
+        <div class="right">
+            <ul class="sub-menu">
+                <li>Season 01<span class="material-icons" style="margin-top: -.2rem;">keyboard_arrow_down</span></li>
+                <li>Characthers</li>
+                <li>Staff</li>
+                <li>Similar Anime</li>
+            </ul>
+            <div class="box-eps">
+                <div class="box-eps-left">
+                    <div class="video-wrap">
+                        <div class="video">
+                            <img src="play.png" alt="">
+                            Play Trailer
+                        </div>
+                    </div>
+                </div>
+                <div class="box-eps-right">
+                    <p class="tit-eps">Episode 001</p>
+                    <h5 class="title-eps">The Lone Cyborg</h5>
+                    <p class="desc-eps">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+                </div>
+            </div>
+            <div class="box-eps">
+                <div class="box-eps-left">
+                    <div class="video-wrap">
+                        <div class="video">
+                            <img src="play.png" alt="">
+                            Play Trailer
+                        </div>
+                    </div>
+                </div>
+                <div class="box-eps-right">
+                    <p class="tit-eps">Episode 001</p>
+                    <h5 class="title-eps">The Lone Cyborg</h5>
+                    <p class="desc-eps">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+                </div>
+            </div>
+            <div class="box-eps">
+                <div class="box-eps-left">
+                    <div class="video-wrap">
+                        <div class="video">
+                            <img src="play.png" alt="">
+                            Play Trailer
+                        </div>
+                    </div>
+                </div>
+                <div class="box-eps-right">
+                    <p class="tit-eps">Episode 001</p>
+                    <h5 class="title-eps">The Lone Cyborg</h5>
+                    <p class="desc-eps">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="container ctn">
+            <div class="footer-left">
+                <ul>
+                    <li>Update</li>
+                    <li>Featured</li>
+                    <li>Anime List</li>
+                    <li>Favorited</li>
+                    <li>Update</li>
+                    <li>Featured</li>
+                    <li>Anime List</li>
+                    <li>Favorited</li>
+                    <li>Update</li>
+                    <li>Featured</li>
+                    <li>Anime List</li>
+                    <li>Favorited</li>
+                </ul>
+            </div>
+            <div class="footer-right">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
